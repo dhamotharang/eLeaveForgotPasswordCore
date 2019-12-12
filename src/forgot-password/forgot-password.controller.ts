52c131f4ea6725f6420c94cfa6858285d73c94fa
Fakhri <fakhrialuwe@rocketmail.com>
2019-12-12 12:30:46 +0800

    log headers

--- src/forgot-password/forgot-password.controller.ts
+++ src/forgot-password/forgot-password.controller.ts
@@ -59,18 +59,18 @@ export class ForgotPasswordController {
     const userAgent = req.headers['user-agent'];
     // console.log(req.connection);
 
-    const requestIp = require('request-ip');
+    // const requestIp = require('request-ip');
 
     let ip;
 
-    // inside middleware handler
-    const ipMiddleware = function (req) {
-      const clientIp = requestIp.getClientIp(req);
-      var splitted = clientIp.split(':');
-      return splitted[splitted.length - 1];
-    };
+    // // inside middleware handler
+    // const ipMiddleware = function (req) {
+    //   const clientIp = requestIp.getClientIp(req);
+    //   var splitted = clientIp.split(':');
+    //   return splitted[splitted.length - 1];
+    // };
 
-    ip = ipMiddleware(req);
+    // ip = ipMiddleware(req);
     // console.log(ip);
     var externalip = require('externalip');
 
@@ -88,19 +88,27 @@ export class ForgotPasswordController {
 
     ip = myIp;
 
-    const dataTemp = () => {
-      return new Promise((resolve, reject) => {
-        this.httpService.get('http://pv.sohu.com/cityjson').subscribe(
-          data => {
-            resolve(data.data);
-          }, err => {
-            return reject(err);
-          }
-        );
-      });
-    }
-    const dataIp = await dataTemp();
-    console.log(dataIp);
+    // const dataTemp = () => {
+    //   return new Promise((resolve, reject) => {
+    //     this.httpService.get('http://pv.sohu.com/cityjson').subscribe(
+    //       data => {
+    //         resolve(data.data);
+    //       }, err => {
+    //         return reject(err);
+    //       }
+    //     );
+    //   });
+    // }
+    // const dataIp = await dataTemp();
+    // console.log(dataIp);
+
+    // var get_non_routable_prefix_list = require('ipware');
+    // var ip_info = get_non_routable_prefix_list();
+    // var ip_data = get_non_routable_prefix_list().get_headers_attribute(req.header, 'X_FORWARDED_FOR');
+    // console.log(ip_info);
+    // console.log(ip_data);
+    console.log(req.headers);
+
 
     let method;
     if (sendEmailDTO.role == 'tenant')
