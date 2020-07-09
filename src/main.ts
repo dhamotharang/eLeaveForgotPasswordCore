f058bf7ede7bb7ef7232fa9b0cdc2eed9104e318
Fakhri <fakhrialuwe@rocketmail.com>
2020-07-09 15:49:25 +0800

    update https setting

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, Logger } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
+require('dotenv').config();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const fs = require('fs');
+  let app;
+  // console.log(process.env.SSL_HTTP);
+  if (process.env.SSL_HTTP == 'true') {
+    // console.log('here');
+    // fs.readFileSync('src/config/certlocal.pfx');
+    const certFile = fs.readFileSync(process.env.SSL_CERT_PATH);
+    const keyFile = fs.readFileSync(process.env.SSL_KEY_PATH);
+    app = await NestFactory.create(AppModule, { cors: true, httpsOptions: { cert: certFile, key: keyFile } });
+  } else {
+    console.log('hero');
+    app = await NestFactory.create(AppModule, { cors: true });
+  }
 
   app.useGlobalPipes(new ValidationPipe(
     {
